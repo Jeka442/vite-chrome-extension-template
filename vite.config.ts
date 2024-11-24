aa94e6f9d02673f9150136e6a013800c5cfcd5f3
Eivgeniy Gudilov <eivgeniygu@payoneer.com>
2024-11-24 09:55:31 +0200

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'extension/**/*',
+          dest: '.',
+        },
+      ],
+    }),
+  ],
+  base: './',
+  build: {
+    outDir: 'dist',
+  },
+})
